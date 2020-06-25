6975d095ffeb52a76b8c1055598040d6790e8717
devyanshuk <devkoirala10@gmail.com>
2020-06-25 23:24:41 +0200

    added build script

--- play.sh
+++ play.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-PacmanGame="Pacman"
+PacmanGame="Pac-man"
 
 while read -r line; do
 	name=$(echo "$line" | rev | cut -d "/" -f1)
+	name=$(echo "$name" | rev)
 	if (("$name" == "$PacmanGame")); then
+		pip install pygame
 		cd $line && python3 main.py 
 		break
 	fi
-done < <(find 2> /dev/null ~ -d -name "Pacman")
+done < <(find 2> /dev/null ~ -d -name "Pac-man")
