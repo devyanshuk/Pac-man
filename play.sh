244838401dec8bc5f512c57ce3e5b62148acb0b5
devyanshuk <devkoirala10@gmail.com>
2020-06-26 00:06:08 +0200

    updates

--- play.sh
+++ play.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 PacmanGame="Pac-man"
+brew install coreutils
+DIR="$(dirname "$(greadlink -f "$0")")"
 
 while read -r line; do
 	name=$(echo "$line" | rev | cut -d "/" -f1)
 	name=$(echo "$name" | rev)
 	if (("$name" == "$PacmanGame")); then
+		echo -e "alias pac-man='cd "$DIR" && python3 main.py'\n$(cat ~/.bash_profile)" > ~/.bash_profile
 		pip install pygame
 		cd $line && python3 main.py 
 		break
